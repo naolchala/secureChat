890f014f586fb868a836c8def5b483f0a6452aa0
Naol Chala <naolchala6@gmail.com>
2024-04-27 00:35:44 +0300

    config: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/poppins";
 import "./index.css";
 import theme from "./config/theme.config.ts";
+import { RouterProvider } from "react-router-dom";
+import router from "./config/router.config.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ChakraProvider theme={theme}>
-			<App />
+			<RouterProvider router={router} />
 		</ChakraProvider>
 	</React.StrictMode>
 );
